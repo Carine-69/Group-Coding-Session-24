dcb7a017b4219623e30e7aae2ae108dfbfd33e19
Carine-69 <155974482+Carine-69@users.noreply.github.com>
2024-02-15 14:20:45 +0200

    Add files via upload
    copying files to a directory

---
+++ move-to-directory.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#move all created files to move-to-directory.sh
+mv main.sh students-list_1023.txt select-email.sh student-email.txt"$Group-Coding-Session-24_id-iq"
+echo "files moved to $Group-Coding-Session-24_id-iq"
+
+
